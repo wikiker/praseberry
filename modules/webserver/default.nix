47bcbb6143d161c4620c5390dc530db0428ac351
Daniel Perout <wikiker@protonmail.com>
2024-03-28 21:59:18 +0100

    Fix redirecting to songs

--- modules/webserver/default.nix
+++ modules/webserver/default.nix
@@ -27,8 +27,11 @@ in
         locations."/" = {
           proxyPass = "http://localhost:${builtins.toString ports.homepage}";
         };
-        locations."/znelky(.*)$" = {
-          proxyPass = "http://localhost:${builtins.toString ports.mympd}$1";
+        locations."/znelky(?<sub_dir>.*)$" = {
+          proxy_set_header X-Real-IP  $remote_addr;
+          proxy_set_header X-Forwarded-For $remote_addr;
+          proxy_set_header Host $host;
+          proxyPass = "http://localhost:${builtins.toString ports.mympd}$sub_dir";
         };
       };
     };
