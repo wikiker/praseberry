5688e8686c8f19652cc0e8d665ef2f4b81054d98
Daniel Perout <wikiker@protonmail.com>
2024-03-28 19:21:34 +0100

    Change source of mympd service

--- modules/songs/default.nix
+++ modules/songs/default.nix
@@ -1,25 +1,25 @@
-{ lib, pkgs, config, ... }:
+{ inputs, lib, pkgs, config, ... }:
 
 with lib;
 let
   mympd_port = config.songs.port;
 in
 {
+  imports = [
+    "${inputs.nixpkgs-unstable}/nixos/modules/services/audio/mympd.nix"
+  ];
+
   options = {
     songs.port = mkOption {
       type = with types; int;
     };
   };
-  
-  config = {
-    environment.systemPackages = with pkgs [
-      mpd
-      unstable.mympd #There is no services.mympd on stable branch
-    ];
 
+  config = {
     services.mpd.enable = true;
     services.mympd = {
       enable = true;
+      package = pkgs.unstable.mympd;
       settings.http_port = mympd_port;
     };
   };
