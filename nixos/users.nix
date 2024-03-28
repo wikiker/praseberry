2dd62089af627b95e2f1abc64524cb209de82725
Daniel Perout <wikiker@protonmail.com>
2024-03-28 13:31:26 +0100

    Fix nix syntax

--- nixos/users.nix
+++ nixos/users.nix
@@ -11,6 +11,6 @@
         # Daníkův klíč
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc7hBSDeDPZ0IsxfHaQZ7Adfug5H1h+dDYu2sML+8Pp wikiker@DanikuvkBucek22.COMFAST"
       ];
-    }
-  }
+    };
+  };
 }
