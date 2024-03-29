966a748281025720f472422438dfef88d240c8ef
Daniel Perout <wikiker@protonmail.com>
2024-03-29 19:01:38 +0100

    Fix static ip

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -6,6 +6,16 @@ in
 {
   networking = {
     useDHCP = false;
-    defaultGateway.address = static_ip;
+
+    interfaces = {
+      enu1u1.ipv4.addresses = [{
+        address = static_ip;
+        # prefixLength = 24;
+      }];
+      wlan0.ipv4.addresses = [{
+        address = static_ip;
+        # prefixLength = 24;
+      }];
+    };
   };
 }
