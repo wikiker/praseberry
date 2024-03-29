79a0332103c4c47d4342d4fd9df806a59b451682
Daniel Perout <wikiker@protonmail.com>
2024-03-29 18:05:30 +0100

    Add public ssh key

--- nixos/users.nix
+++ nixos/users.nix
@@ -10,6 +10,8 @@
       openssh.authorizedKeys.keys = [
         # Daníkův klíč
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKc7hBSDeDPZ0IsxfHaQZ7Adfug5H1h+dDYu2sML+8Pp wikiker@DanikuvkBucek22.COMFAST"
+        # Petrův klíč
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7085N6oPvLplV8f4B9qILS8GXBPXi4Dk0kaR7I04Gy lolec@Martin"
       ];
     };
   };
